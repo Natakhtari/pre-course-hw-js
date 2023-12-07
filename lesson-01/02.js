89d7e70ac9632a26ce6d1f144d51a388fd346bf0
vage8 <vage123580@gmail.com>
2023-12-07 16:46:46 +0400

    odd or even

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number2 = Math.floor(Math.random() * 100 );
+
+if (number2 % 2 === 0) {
+    console.log(`${number2} : Это четное число`);
+} else {
+    console.log(`${number2} : Это нечетное число`);
+}
